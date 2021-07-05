45a418520f8d34f70d7178bb4d2a0b696db7a8a9
harry77j <fhwik1993@gmail.com>
2021-07-05 14:57:57 +0900

    issue #1

--- hello.py
+++ hello.py
@@ -2,6 +2,9 @@
 
 import sys
 
-name = sys.argv[1]
+name = sys.argv[1] # 리스트에 계속 넣어준다
+name2 =sys.argv[2]
 
-print(f"Hello {name}")
+print(f"Hello {name} {name2}") # name에 받은것을 {name}에 넣어준다
+
+print(sys.argv)
